7380ed1c5e1ddacb841d1e715b4d269838b7a15b
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-05-19 10:55:09 +0530

    add userSubmitted

--- src/pages/quize.jsx
+++ src/pages/quize.jsx
@@ -1,6 +1,7 @@
 import { addDoc, collection, getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { db } from '../../firebase';
+import user from '../../utils/user_services';
 
 const Quiz = () => {
     const [quizzes, setQuizzes] = useState([]);
@@ -9,6 +10,7 @@ const Quiz = () => {
     const [userName, setUserName] = useState('');
     const [timeLeft, setTimeLeft] = useState(15);
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isUserExist,setUserExist]=useState(false)
 
     useEffect(() => {
         const fetchQuizzes = async () => {
@@ -52,46 +54,58 @@ const Quiz = () => {
         await addDoc(collection(db, 'results'), { userName, score });
     };
 
+    const checkUserpass = async ()=>{
+        const check= await user.isUserSubmitted(userName)
+        if (check){
+            setUserExist(true)
+        }
+
+    }
+
     return (
-        <div className=' bg-black w-full h-screen text-white'>
-            {!isSubmitted ? (
-                <div className=''>
-                    {currentQuizIndex === -1 && (
-                        <div className=' m-auto w-fit relative top-96 flex flex-wrap items-center justify-center'>
-                            <input
-                                type="text"
-                                placeholder="Enter your name"
-                                value={userName}
-                                onChange={(e) => setUserName(e.target.value)}
-                                className=' text-4xl p-4'
-                            />
-                            <button style={{ backgroundColor: "#FCE300", color: "black" }} className=' p-4 text-4xl hover:bg-black hover:text-white active:bg-white active:text-black' onClick={() => setCurrentQuizIndex(0)}>Start Quiz</button>
-                        </div>
-                    )}
-                    {currentQuizIndex >= 0 && currentQuizIndex < quizzes.length && (
-                        <div className=' m-auto w-fit  relative top-24  '>
-                            <h2 className='text-center' style={{ fontSize: "86px" }}> 00 : {timeLeft}</h2>
-                            <h3 style={{ fontSize: "36px", maxWidth: "1220px" }} className='m-auto'>{quizzes[currentQuizIndex].question}</h3>
-                            <div className='ml-40 mt-10'>
-                                {quizzes[currentQuizIndex].options.map((option, index) => (
-                                    <button className='p-5 m-2 active:bg-white active:text-black' style={{
-                                        border: "3px #FCE300 solid", borderRadius: "12px", width: "600px", height: "96px", fontSize: "32px"
-                                    }} key={index} onClick={() => handleAnswer(option.isCorrect)}>
-                                        {option.answer}
-                                    </button>
-                                ))}
-                                <button onClick={handleNext} style={{ width: "457px", height: "80px", borderRadius: "12px", backgroundColor: "#FCE300", fontSize: "36px" }} className=' text-black m-auto ml-96'>Next Question</button>
+            <div className=' bg-black w-full h-screen text-white'>
+                {!isSubmitted ? (
+                    <div className=''>
+                        {currentQuizIndex === -1 ? (
+                            <div className=' m-auto w-fit relative top-96 flex flex-wrap items-center justify-center'>
+                                <input
+                                    type="text"
+                                    placeholder="Enter your name"
+                                    value={userName}
+                                    onChange={(e) => setUserName(e.target.value)}
+                                    className=' text-4xl p-4'
+                                />
+                                <button style={{ backgroundColor: "#FCE300", color: "black" }} className=' p-4 text-4xl hover:bg-black hover:text-white active:bg-white active:text-black' onClick={() => setCurrentQuizIndex(0)}>Start Quiz</button>
                             </div>
-                        </div>
-                    )}
-                </div>
-            ) : (
-                <div className=' text-center text-4xl'>
-                    <h2>Quiz completed!</h2>
-                    <p>{userName}, your score is: {score}</p>
-                </div>
-            )
-            }
+                        ) : isUserExist ? (
+                            <div>
+                                <h2>You are already participated.</h2>
+                            </div>
+                        ):(
+                            currentQuizIndex >= 0 && currentQuizIndex < quizzes.length && (
+                                <div className=' m-auto w-fit  relative top-24  '>
+                                    <h2 className='text-center' style={{ fontSize: "86px" }}> 00 : {timeLeft}</h2>
+                                    <h3 style={{ fontSize: "36px", maxWidth: "1220px" }} className='m-auto'>{quizzes[currentQuizIndex].question}</h3>
+                                    <div className='ml-40 mt-10'>
+                                        {quizzes[currentQuizIndex].options.map((option, index) => (
+                                            <button className='p-5 m-2 active:bg-white active:text-black' style={{
+                                                border: "3px #FCE300 solid", borderRadius: "12px", width: "600px", height: "96px", fontSize: "32px"
+                                            }} key={index} onClick={() => handleAnswer(option.isCorrect)}>
+                                                {option.answer}
+                                            </button>
+                                        ))}
+                                        <button onClick={handleNext} style={{ width: "457px", height: "80px", borderRadius: "12px", backgroundColor: "#FCE300", fontSize: "36px" }} className=' text-black m-auto ml-96'>Next Question</button>
+                                    </div>
+                                </div>
+                                )
+                            )}
+                    </div>
+                ) : (
+                    <div className=' text-center text-4xl'>
+                        <h2>Quiz completed!</h2>
+                        <p>{userName}, your score is: {score}</p>
+                    </div>
+                )}
         </div >
     );
 };
