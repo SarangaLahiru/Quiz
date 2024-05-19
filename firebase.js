830fc9de78e6eb1d2eafa21705678c400437d4c7
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-05-19 08:10:43 +0530

    first commit

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDtRh1u_javbvFb1CoPq4199UYza3YyLzM",
     authDomain: "quiz-d58b7.firebaseapp.com",
