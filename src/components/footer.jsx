3115cc75b1006dc8814f277ee797d8022c1bc3bd
Pasindu Pathum <gappathum@std.appsc.sab.ac.lk>
2024-06-11 15:37:41 +0530

    Guidelines page

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer  () {
+  return (
+<div className='foot'>
+      <div className='foot1'></div>
+      <div className='foot2'>
+        <div className='scrolling-text' style={{color: 'black'}}>
+            QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | QWHIZ  | 
+                   </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Footer;
